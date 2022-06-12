f971206d98227538eda7acd5c8a8f70658dd84f8
Henrique Boiteux <92280899+hboiteux25@users.noreply.github.com>
2022-06-12 11:55:58 -0300

    commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+//INICIAL DATA
+let currentColor = 'black'
+let screen = document.queryCommandValue('#tela');
+let ctx = screen.getContext('2d');
+
+//EVENTS
+document.querySelectorAll('.colorArea .color').forEach(item =>{
+   item.addEventListener('click', colorClickEvent); 
+});
+
+
+//FUNCTIONS
+function colorClickEvent(e){
+    let color = e.target.getAttribute('data-color');
+    currentColor = color;
+
+    document.querySelector('.color.active').classList.remove('active')
+    e.target.classList.add('active');
+}
+
+
+
